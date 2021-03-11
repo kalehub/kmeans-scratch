0b02bc18723a1fb6f1b3cf7a49ff47a336bce89d
kalehub <=>
2021-03-11 19:51:00 +0800

    finish batch-01

--- main.py
+++ main.py
@@ -7,41 +7,62 @@ import matplotlib.pyplot as plt
 def read_dataset(dataset_dir):
     return np.genfromtxt(dataset_dir, delimiter=';')
 
-def init_cluster(centroid, data, k):
-    all_euclidean = np.array([])
-    # for this project only!
-    for i in range(k):
-        # membuat array dengan elemen yang berulang
-        centro = np.array(np.tile(centroid[i], (data.shape[0],1)))
-        # proses pengurangan
-        subt_arr = abs(np.subtract(centro, data))
-        squared_arr = np.square(subt_arr)
-        euclidean_distance = np.sqrt(squared_arr.sum(axis=1))
-        all_euclidean = np.append(all_euclidean, euclidean_distance)
-        print(euclidean_distance)
-    
-    # all_euclidean = all_euclidean.reshape(15,1)
-    final_eucl = np.ones((15,3)) 
-    all_euclidean = np.reshape(all_euclidean,(3,-1))
-    for _ in all_euclidean:
-        temp = _.reshape(15,-1)
-        final_eucl = np.hstack((final_eucl, temp))
-    final_eucl = final_eucl[:,3:]
-    print(f'jarak euclidean :\n {final_eucl}')
-    # mengambil nilai index minimal menggunakan np.argmin
-    euclidean_min = np.hstack((final_eucl, np.argmin(final_eucl, axis=1).reshape(15,-1)))
-    data_with_cluster = np.hstack((data, np.argmin(final_eucl, axis=1).reshape(15,-1))) 
-    print(f'data dengan cluster :\n {data_with_cluster}')
+def init_cluster(cen, data, k):
+    result = np.array([])
+    centroid = cen
+    itr = 0
+    while(True):
+        all_euclidean = np.array([])
+        # for this project only!
+        for i in range(k):
+            # membuat array dengan elemen yang berulang
+            centro = np.array(np.tile(centroid[i], (data.shape[0],1)))
+            # proses pengurangan
+            subt_arr = abs(np.subtract(centro, data))
+            squared_arr = np.square(subt_arr)
+            euclidean_distance = np.sqrt(squared_arr.sum(axis=1))
+            all_euclidean = np.append(all_euclidean, euclidean_distance)
+            print(euclidean_distance)
+        
+        # all_euclidean = all_euclidean.reshape(15,1)
+        final_eucl = np.ones((15,3)) 
+        all_euclidean = np.reshape(all_euclidean,(3,-1))
+        for _ in all_euclidean:
+            temp = _.reshape(15,-1)
+            final_eucl = np.hstack((final_eucl, temp))
+        final_eucl = final_eucl[:,3:]
+        # print(f'jarak euclidean :\n {final_eucl}')
+        # mengambil nilai index minimal menggunakan np.argmin
+        euclidean_min = np.hstack((final_eucl, np.argmin(final_eucl, axis=1).reshape(15,-1)))
+        data_with_cluster = np.hstack((data, np.argmin(final_eucl, axis=1).reshape(15,-1))) 
+        print(f'data dengan cluster :\n {data_with_cluster}')
 
-    # deklarasi array cluster baru
-    new_cluster = np.zeros_like(centroid)
-    # menentukan kluster baru
-    for k in range(0, (data_with_cluster[0].shape[0]-1)):
-        # print(data_with_cluster[np.where(data_with_cluster[:,-1] == k)])
-        array_cluster = data_with_cluster[np.where(data_with_cluster[:,-1] == k)] 
-        avg_cluster = np.divide(np.sum(array_cluster,axis=0), array_cluster.shape[0])
-        new_cluster[k] = avg_cluster[:3]
-    print(new_cluster)
+        # deklarasi array cluster baru
+        new_cluster = np.zeros_like(centroid)
+        # menentukan kluster baru
+        for k in range(0, (data_with_cluster[0].shape[0]-1)):
+            # print(data_with_cluster[np.where(data_with_cluster[:,-1] == k)])
+            array_cluster = data_with_cluster[np.where(data_with_cluster[:,-1] == k)] 
+            avg_cluster = np.divide(np.sum(array_cluster,axis=0), array_cluster.shape[0])
+            new_cluster[k] = avg_cluster[:3]
+        print(f'cluster baru:\n{new_cluster}')
+        itr+=1
+        centroid = new_cluster
+    
+        if (new_cluster == centroid).all():
+            result = data_with_cluster
+            print('Nilai UTS | Nilai Tugas | Nilai UAS| Cluster |')
+            print(result)
+            print(f'clustering selesai pada iterasi ke-{itr}')
+            print('proses selesai')
+
+            
+            break
+
+        
+
+
+
 
 def main():
     # membaca dataset
